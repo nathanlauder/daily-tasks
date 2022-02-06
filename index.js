c56e2d53290f84e54e0a5e12059cf9884f30cbd4
nathanlauder <laudern42@gmail.com>
2022-02-06 01:20:51 -0600

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+import crypto from './apiInteractions/crypto.js';
+import calendar from './apiInteractions/calendar.js';
+// import { colors } from './colors.js';
+
+// const {
+//   blue, yellow, green, error
+// } = colors;
+
+calendar();
+crypto();
