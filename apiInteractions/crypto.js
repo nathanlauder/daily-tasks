d760bf87a4a7b09ba31c6616b85fd303bfbf2f6f
nathanlauder <laudern42@gmail.com>
2022-02-06 01:21:45 -0600

    add crypto api calls

---
+++ apiInteractions/crypto.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { colors } from '../colors.js';
+import { cryptoConversions } from '../config/cryptoConversions.js';
+
+const {
+  blue, green, error
+} = colors;
+
+const call = () => {
+  const endpoint = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=NEAR,ETH,MKR,COMP,GRT,XLM,CELO,BAT,BTC,BAND&tsyms=USD&api_key=${process.env.REACT_APP_CRYPTO_KEY}`;
+  try {
+    return axios.get(endpoint);
+  } catch (err) {
+    return err;
+  }
+};
+
+const crypto = () => {
+  let totalPortfolio = 0.0;
+  call()
+    .then(({ data }) => {
+      Object.keys(data).forEach((token) => {
+        totalPortfolio += cryptoConversions[token] * data[token].USD;
+        console.log(`${blue(token)}: ${(cryptoConversions[token] * data[token].USD).toFixed(2)}`);
+      });
+      console.log(`Total: ${green(totalPortfolio.toFixed(2))}`);
+    })
+    .catch((err) => console.log(error(err)));
+};
+
+export default crypto;
