55db29e8e4654856e4f5b8fb3552c1e2cc823c9b
nathanlauder <laudern42@gmail.com>
2022-02-06 01:20:18 -0600

    add more colors

---
+++ colors.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-unresolved
+import chalk from 'chalk';
+
+export const colors = {
+  blue: chalk.bold.hex('#00dcf4'),
+  yellow: chalk.bold.yellow,
+  error: chalk.hex('#ff001a'),
+  green: chalk.bold.hex('#00ff00'),
+};
